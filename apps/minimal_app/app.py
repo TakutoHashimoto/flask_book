123f8d6018e31c8a580c51d79dc3b6408b3fcd6b
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-02 23:32:57 +0900

    create apps

---
+++ apps/minimal_app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, Flask book!"
+
+
+# ターミナル上でpython app.pyで実行する場合は以下のコードを書く必要がある。
+# if __name__ == "__main__":
+#     app.run(debug=True)
