b998bfa39e79f9a4044c9c69e36f6710cb6a4567
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-05 11:59:42 +0900

    ブラウザに任意の文字列が表示されるように変更

--- apps/minimal_app/app.py
+++ apps/minimal_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
@@ -8,10 +9,10 @@ def index() -> str:
     return "Hello, Flask book!"
 
 
-@app.route("/hello", methods=["GET", "POST"])
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
 # app.get("/hello")と書くこともできる
-def hello() -> str:
-    return "Hello, World!"
+def hello(name: str) -> str:
+    return f"Hello, {name}!"
 
 
 # ターミナル上でpython app.pyで実行する場合は以下のコードを書く必要がある。
