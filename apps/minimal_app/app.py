5d4a05a8c3570357d2843c4450904c1ff8dbe00a
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-06 01:57:54 +0900

    リクエストコンテキストを追加

--- apps/minimal_app/app.py
+++ apps/minimal_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, current_app, g
+from flask import Flask, render_template, url_for, current_app, g, request
 
 
 app = Flask(__name__)
@@ -41,6 +41,10 @@ with app.test_request_context():
     print(url_for("hello-endpoint", name="world"))
     # /name/ichiro?page=1
     print(url_for("show_name", name="ichiro", page=1))
+
+with app.test_request_context("users?update=true"):
+    print(request.args.get("updated"))
+    # => true
 
 
 # ターミナル上でpython app.pyで実行する場合は以下のコードを書く必要がある。
