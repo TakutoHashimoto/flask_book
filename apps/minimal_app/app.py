e5f1737837bcc70e98bdf893c40803b32512eea0
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-05 12:13:04 +0900

    jinjaテンプレートを使ってHTMLをレンダリングする関数を追加

--- apps/minimal_app/app.py
+++ apps/minimal_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -13,6 +13,11 @@ def index() -> str:
 # app.get("/hello")と書くこともできる
 def hello(name: str) -> str:
     return f"Hello, {name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name: str) -> "html":
+    return render_template("index.html", name=name)
 
 
 # ターミナル上でpython app.pyで実行する場合は以下のコードを書く必要がある。
